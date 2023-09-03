46fb3628c11467d0f548aaed4272039d1424d2b7
김우빈 <kwb020312@naver.com>
2023-09-03 21:40:15 +0900

    Feat: Adding Earth Texture

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,12 +1,34 @@
 import { Fragment } from "react";
+import { OrbitControls } from "@react-three/drei";
+
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 const Earth = (props) => {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
   return (
     <Fragment>
       <ambientLight intensity={1} />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial color="red" />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
       </mesh>
     </Fragment>
   );
