eeb25650d2f95d003cf1c1a4b83c2e987aadbd28
김우빈 <kwb020312@naver.com>
2023-09-04 21:24:24 +0900

    Feat: Add pointLight

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -17,7 +17,8 @@ const Earth = (props) => {
 
   return (
     <Fragment>
-      <ambientLight intensity={1} />
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color={"#f6f3ea"} position={[2, 0, 2]} intensity={30} />
       <Stars
         radius={300}
         depth={60}
@@ -39,7 +40,12 @@ const Earth = (props) => {
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
         <OrbitControls
           enableZoom={true}
           enablePan={true}
