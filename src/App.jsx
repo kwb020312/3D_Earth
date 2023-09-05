1d47dd59898c61177b0d42fcf63c78aa246f9003
Chobby <46777310+kwb020312@users.noreply.github.com>
2023-09-05 12:15:19 +0900

    Fix: 전체화면 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { styled } from "styled-components";
-import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Earth from "./components/earth";
